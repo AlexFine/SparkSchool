91437c3a3948c397393ee7d6779e43543df3bcaa
Sam Schickler <gadgetsam@gmail.com>
2016-06-05 12:18:32 -0700

    adding js

--- ani-theme-master/app/scripts/controllers/login.js
+++ ani-theme-master/app/scripts/controllers/login.js
@@ -9,12 +9,34 @@
  */
 angular.module('yapp')
   .controller('LoginCtrl', function($scope, $location) {
+    function loadJSON(callback) {
 
-    $scope.submit = function() {
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', 'scripts/page.json', true); // Replace 'my_data' with the path to your file
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+          callback(xobj.responseText);
+        }
+      };
+      xobj.send(null);
+    }
 
-      $location.path('/dashboard');
-
-      return false;
+    loadJSON(function(response) {
+      // Parse JSON string into object
+      $scope.page = JSON.parse(response);
+      $scope.name = $scope.page.name_of_school
+      console.log($scope.name);
+      $scope.load()
+    });
+    $scope.load = function(){
+      console.log($scope.name)
+      $scope.submit()
+    }
+    // $scope.page = JSON.parse(response);
+    // $scope.name = $scope.page.name_of_school
+    $scope.submit = function(t) {
     }
 
   });
